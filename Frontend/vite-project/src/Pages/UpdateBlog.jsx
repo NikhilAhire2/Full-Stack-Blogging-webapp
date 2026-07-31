c64b9a4d2f5ea6ad64c3bb540f5b3d1b1e7c5772
Nikhil Ahire <ahirenikhil183@gmail.com>
2026-07-31 19:16:19 +0530

    Fix Button import path in UpdateBlog.jsx2

--- Frontend/vite-project/src/Pages/UpdateBlog.jsx
+++ Frontend/vite-project/src/Pages/UpdateBlog.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 //import { Button } from '../components/ui/Button'
-import {Button} from "../components/ui/Button";
+import {Button} from "../components/ui/button.jsx";
 import { Label } from '@radix-ui/react-label'
 import { Input } from '../components/ui/input'
 import { Card } from "../components/ui/card"
